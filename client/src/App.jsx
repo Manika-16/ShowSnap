ef37aef9f82138a8d1adc4d9b24cae5d6639bc41
Manika Jain <manika0816@gmail.com>
2025-06-22 00:56:16 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/home";
+import Movie from "./pages/Movies";
+import MyBooking from "./pages/mybooking";
+import Favourite from "./pages/Favourite";
+import MovieDetails from "./pages/moviedetails";
+import SeatLayout from "./pages/SeatLayout";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddShows from "./pages/admin/AddShows";
+import ListShows from "./pages/admin/ListShows";
+import ListBookings from "./pages/admin/ListBookings";
+
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+
+const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBooking />} />
+        <Route path="/favourite" element={<Favourite />} />
+        <Route
+          path="/admin/*"
+          element={ <Layout />
+            // user ? (
+            //   <Layout />
+            // ) : (
+            //   <div className="min-h-screen flex justify-center items-center">
+            //     <SignIn fallbackRedirectUrl={"/admin"} />
+            //   </div>
+            // )
+          }
+        >
+          <Route index element={<Dashboard />} />
+          <Route path="add-shows" element={<AddShows />} />
+          <Route path="list-shows" element={<ListShows />} />
+          <Route path="list-bookings" element={<ListBookings />} />
+        </Route>
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+};
+
+export default App;
