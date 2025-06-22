b4b6da30d481800e17327dc8c1744b43bc149785
Manika Jain <manika0816@gmail.com>
2025-06-22 15:42:46 +0530

    admin functionality

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkMiddleware } from '@clerk/express';
 import { handleClerkWebhook } from './controllers/clerkWebhooks.js';
 import showRouter from './routes/showRouter.js';
 import bookingRouter from './routes/bookingRouter.js';
+import adminRouter from './routes/adminRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -25,14 +26,13 @@ app.get('/', (req, res) => res.send('Server is Live!'));
 app.use('/api/show', showRouter);
 
 // Uncomment these when needed
-// import adminRouter from './routes/adminRoutes.js';
 // import userRouter from './routes/userRoutes.js';
 // import { stripeWebhooks } from './controllers/stripeWebhooks.js';
 
 // Stripe Webhooks Route
 // app.use('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 app.use('/api/booking', bookingRouter);
-// app.use('/api/admin', adminRouter);
+app.use('/api/admin', adminRouter);
 // app.use('/api/user', userRouter);
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
