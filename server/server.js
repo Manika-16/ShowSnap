84e1390f902b3fb390598af1b698528a5a1d99c7
Manika Jain <manika0816@gmail.com>
2025-06-22 15:49:45 +0530

    user functionality

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { handleClerkWebhook } from './controllers/clerkWebhooks.js';
 import showRouter from './routes/showRouter.js';
 import bookingRouter from './routes/bookingRouter.js';
 import adminRouter from './routes/adminRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -26,13 +27,12 @@ app.get('/', (req, res) => res.send('Server is Live!'));
 app.use('/api/show', showRouter);
 
 // Uncomment these when needed
-// import userRouter from './routes/userRoutes.js';
 // import { stripeWebhooks } from './controllers/stripeWebhooks.js';
 
 // Stripe Webhooks Route
 // app.use('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 app.use('/api/booking', bookingRouter);
 app.use('/api/admin', adminRouter);
-// app.use('/api/user', userRouter);
+app.use('/api/user', userRouter);
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
